7b1731ae75442d3c65d66e452552bd81c65ca0b1
Furysmash <63118634+Furysmash@users.noreply.github.com>
2020-04-10 10:01:48 +0530

    added bounceOff algorithm

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ var fixedRect,movingRect
 
 var rect1,rect2,rect3,rect4
 
-
+var rect5,rect6
 
 function setup() {
   createCanvas(1200,800);
@@ -18,6 +18,12 @@ function setup() {
   rect3.shapeColor = "green";
   rect4 = createSprite (400,100,50,50);
   rect4.shapeColor = "green";
+  rect5 = createSprite (600,100,50,50);
+  rect5.shapeColor = "blue";
+  rect5.velocityY = 5;
+  rect6 = createSprite (600,800,50,50);
+  rect6.shapeColor = "yellow";
+  rect6.velocityY = -5;
 }
 
 function draw() {
@@ -36,6 +42,8 @@ function draw() {
     rect1.shapeColor = "green";
   }
 
+  bounceOff (rect5,rect6);
+
   drawSprites();
 }
 
@@ -49,7 +57,17 @@ function isTouching (object1,object2) {
   } else {
    return false;
   }
+}
+function bounceOff (object1,object2) {
+  if ( object1.y - object2.y < (object1.height + object2.height)/2 && 
+  object2.y - object1.y < (object1.height + object2.height)/2 )  {
+    object1.velocityY = object1.velocityY*(-1);
+    object2.velocityY = object2.velocityY*(-1);
+  }
 
-
-
+   if (object1.x - object2.x < (object1.width + object2.width)/2 && 
+   object2.x - object1.x < (object1.width + object2.width)/2)  {
+    object1.velocityX = object1.velocityX*(-1);
+    object2.velocityX = object2.velocityX*(-1);
+  }
 }
